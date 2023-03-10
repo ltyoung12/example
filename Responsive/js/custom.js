ccef99cd1b605facfdec547b52f2ece8821306dc
ltyoung12 <gogod65@naver.com>
2023-03-10 14:23:26 +0900

    1.6

---
+++ Responsive/js/custom.js
@@ -0,0 +1,123 @@
+$(function () {
+  // 스크롤이벤트
+  $(window).scroll(function () {
+    var scrolltop = $(document).scrollTop();
+    console.log(scrolltop);
+    var brown = $(".header").css("background-color", "brown");
+    var trans = $(".header").css("background-color", "transparent");
+
+    // 헤더
+    if (scrolltop > 0 && trans) {
+      $(".header").css("background-color", "brown");
+      $(".header_navi > li").addClass("on");
+      $(".header_logo > h1 > a").addClass("on");
+    } else if (scrolltop <= 0 && brown) {
+      $(".header").css("background-color", "transparent");
+      $(".header_navi > li").removeClass("on");
+      $(".header_logo > h1 > a").removeClass("on");
+    }
+
+    // about
+
+    if (scrolltop >= 842 && scrolltop < 1396) {
+      $(".about").addClass("orange");
+    } else {
+      $(".about").removeClass("orange");
+    }
+    // services
+    if (scrolltop >= 1396 && scrolltop < 1933) {
+      $(".services").addClass("orange");
+    } else {
+      $(".services").removeClass("orange");
+    }
+    // portfolio
+    if (scrolltop >= 1933 && scrolltop < 2513) {
+      $(".portfolio").addClass("orange");
+    } else {
+      $(".portfolio").removeClass("orange");
+    }
+    // contact
+    if (scrolltop >= 2513) {
+      $(".contact").addClass("orange");
+    } else {
+      $(".contact").removeClass("orange");
+    }
+  });
+
+  $(document).ready(function () {
+    $(".about").click(function () {
+      $("html").animate({ scrollTop: 900 }, 0.1);
+    });
+    $(".services").click(function () {
+      $("html").animate({ scrollTop: 1396 }, 0.1);
+    });
+    $(".portfolio").click(function () {
+      $("html").animate({ scrollTop: 1933 }, 0.1);
+    });
+    $(".contact").click(function () {
+      $("html").animate({ scrollTop: 2513 }, 0.1);
+    });
+
+    var headtop = $(".header").offset();
+    console.log(headtop);
+    if (headtop >= 0) {
+      $(".header").css("background-color", "brown");
+    } else if (headtop <= 0 && brown) {
+      $(".header").css("background-color", "transparent");
+    }
+  });
+
+  $(window).resize(function () {
+    var width = window.innerWidth;
+    if (width < 767.98) {
+      $(".header_navi").css("display", "none");
+    }
+    if (width > 767.98) {
+      $(".header_navi").css("display", "flex");
+    }
+  });
+  //   버튼 클릭 시 네비창
+  $(".button").click(function () {
+    if ($(".header_navi").css("display") == "none") {
+      $(".header_navi").css("display", "flex");
+    } else if ($(".header_navi").css("display") == "flex") {
+      $(".header_navi").css("display", "none");
+    }
+  });
+
+  // 컨텐츠4 이벤트
+
+  $(".con4_box").click(function () {
+    $(".con4_picture_slide_wrapper").css("display", "block");
+  });
+
+  $(".prev").click(function () {
+    var last = $(".picture_box_list > div:last");
+    $(".picture_box_list > div").hide();
+    $(".picture_box_list").css("left", "-50px");
+    $(".picture_box_list").animate({ left: "0px" }, 400);
+    $(".picture_box_list > div:last").fadeIn(350);
+    $(".picture_box_list").prepend(last);
+  });
+
+  $(".next,.picture_box_list > div").click(function () {
+    var first = $(".picture_box_list > div:first");
+    $(".picture_box_list > div").hide();
+    $(".picture_box_list").css("left", "50px");
+    $(".picture_box_list").animate({ left: "0px" }, 400);
+    $(".picture_box_list").append(first);
+    $(".picture_box_list > div:first").fadeIn(350);
+  });
+
+  $(".close > i").click(function () {
+    $(".con4_picture_slide_wrapper").css("display", "none");
+  });
+
+  var pic = $(".con4_picture_slide_box").get(0);
+  $(".con4_picture_slide_wrapper").click(function (e) {
+    console.log(e.target);
+    if (e.target === pic) {
+      $(".con4_picture_slide_wrapper").css("display", "none");
+    }
+  });
+});
